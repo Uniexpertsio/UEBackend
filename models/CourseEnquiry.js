826c353389fb797c840fe0c6cee711c30da8e11d
rajsayala <88704909+rajsayala@users.noreply.github.com>
2024-01-18 13:36:56 +0530

    Update CourseEnquiry.js
    new model to build service for courseEnquiry

--- models/CourseEnquiry.js
+++ models/CourseEnquiry.js
@@ -1,1 +1,16 @@
 
+IntakeId: {{}}
+PartnerAccountId : {{}}
+PartnerUserId: {{}}
+ProgrammeId: {{}}
+SchoolId: {{}}
+StudentId: {{}}
+CourseEnquiryName: {{}}
+CreatedBy: {{}}
+Currency: {{}}
+LastModifiedBy: {{}}
+Owner: {{}}
+Status: {{}}
+CourseEnquiryExternalId: {{}}
+LockRecord: {{}}
+RecordID: {{}}
