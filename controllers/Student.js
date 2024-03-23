959c16abc905b8e3bcb61a533db1d87cb6722405
Moni kumari <moni.kumari@uniexperts.io>
2024-03-23 08:04:14 +0530

    remove schedule meeting from student

--- controllers/Student.js
+++ controllers/Student.js
@@ -385,19 +385,6 @@ class StudentController {
               res.status(500).json({ error: error.message });
             }
           };
-
-          scheduleMeeting = async (req, res) => {
-            try {
-              const { id } = req.user;
-              const body = req.body;
-              const result = await this.studentService.createMeeting(id, body);
-              res.status(200).json(result);
-            } catch (error) {
-              console.error(error);
-              res.status(500).json({ error: 'Internal Server Error' });
-            }
-          }
-        
 }
   
   module.exports = StudentController;
