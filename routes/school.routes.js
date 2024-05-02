f596b55c7442fe185ede8d49bbfe3f5785403683
Lakshivnitin <nitin.kumar@uniexperts.io>
2024-05-02 19:05:33 +0530

    worked on saving image

--- routes/school.routes.js
+++ routes/school.routes.js
@@ -1,14 +1,33 @@
 const express = require("express");
-const SchoolController =  require("../controllers/School");
-const Middleware = require("../controllers/Middleware")
+const SchoolController = require("../controllers/School");
+const Middleware = require("../controllers/Middleware");
 
 const router = express.Router();
 const schoolController = new SchoolController();
 
-router.put("/", Middleware.checkAuth, schoolController.addOrUpdateSchool.bind(schoolController));
-router.get("/", Middleware.checkAuth, schoolController.getAllSchool.bind(schoolController));
-router.get("/:schoolId", Middleware.checkAuth, schoolController.getSchool.bind(schoolController));
-router.get("/by/country-state-or-school-type", Middleware.checkAuth, schoolController.getSchoolByCountryStateOrSchoolType.bind(schoolController));
-router.get("/programList/:schoolId", schoolController.getSchoolProgram.bind(schoolController));
+router.put(
+  "/",
+  Middleware.checkAuth,
+  schoolController.addOrUpdateSchool.bind(schoolController)
+);
+router.get(
+  "/",
+  Middleware.checkAuth,
+  schoolController.getAllSchool.bind(schoolController)
+);
+router.get(
+  "/:schoolId",
+  Middleware.checkAuth,
+  schoolController.getSchool.bind(schoolController)
+);
+router.get(
+  "/by/country-state-or-school-type",
+  Middleware.checkAuth,
+  schoolController.getSchoolByCountryStateOrSchoolType.bind(schoolController)
+);
+router.get(
+  "/programList/:schoolId",
+  schoolController.getSchoolProgram.bind(schoolController)
+);
 
 module.exports = router;
