1fffbc1ba6ad5d26d037c23f96a1156070207355
Moni kumari <moni.kumari@uniexperts.io>
2024-05-06 17:21:13 +0530

    comment and reply comment api changed as per frontend requirement

--- models/Comment.js
+++ models/Comment.js
@@ -3,15 +3,14 @@ const mongoose = require("mongoose");
 const CommentSchema = new mongoose.Schema(
   {
     message: { type: String, required: true },
+    name: { type: String, required: false },
     userId: { type: String, required: true },
     attachment: { type: String, required: false, default: "" },
     relationId: { type: String, required: true },
     externalId: { type: String, required: true },
     isVideo: { type: Boolean, required: false, default: false },
-    salesforceId:{type:String,required:false},
-    // replyComment: [{type: mongoose.Schema.Types.ObjectId,
-    // ref: 'ReplyComment'
-    // }],
+    salesforceId: { type:String, required:false },
+    isReply: { type: Boolean, default: false},
   },
   { timestamps: true }
 );
