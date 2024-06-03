1618ef9af846f0a2db358a763ef71a9570101501
Moni kumari <moni.kumari@uniexperts.io>
2024-06-03 17:28:52 +0530

    eligibility schema changes

--- utils/emailValidator.js
+++ utils/emailValidator.js
@@ -4,7 +4,7 @@ function validateEmail(email) {
   return emailValidator.validate(email);
 }
 function returnField(examType) {
-  if (examType === "12th Standard English Mark") {
+  if (examType === "12th STD. English mark") {
     return "Pte_Gmat_12th_Total_Marks_of_English__c";
   } else if (examType === "GRE") {
     return "Gre_Analytical_reasoning_Percentile__c";
